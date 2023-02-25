55f391f44493cc52d4ac5f0cf98d49c9a2dda209
Parit Bhardwaj <paritbhardwaj@outlook.com>
2023-02-25 12:22:22 +0530

    Protected Firebase Config!

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDq4ekD7_OP9f2uykGrDiNH24PnaMGrjSE",
-  authDomain: "shoptree-4a69c.firebaseapp.com",
-  projectId: "shoptree-4a69c",
-  storageBucket: "shoptree-4a69c.appspot.com",
-  messagingSenderId: "1067726463868",
-  appId: "1:1067726463868:web:d95b6a6a4dc63d1ded3af9",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export const app = initializeApp(firebaseConfig);
